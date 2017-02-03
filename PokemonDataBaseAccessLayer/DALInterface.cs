22366ac8226eb47adb0a642ef4fc96d9cd177f16
SimVil <simon.vilmin@gmail.com>
2017-02-03 13:58:50 +0100

    added a DAL interface

---
+++ PokemonDataBaseAccessLayer/DALInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonTournamentEntities;
+
+namespace PokemonDataBaseAccessLayer
+{
+    public abstract class DALInterface
+    {
+        public abstract List<Pokemon> GetPokemonList();
+
+        public abstract void AddPokemon(Pokemon n);
+
+        public abstract void DeletePokemon(Pokemon d);
+
+        public abstract void UpdatePokemon(Pokemon u);
+
+        public abstract List<Stade> GetStade();
+
+        public abstract void a();
+
+        public abstract List<Match> GetMatch();
+
+        public abstract void List<Element>();
+
+        
+
+        public abstract void ShowAllRecords();
+
+    }
+}
