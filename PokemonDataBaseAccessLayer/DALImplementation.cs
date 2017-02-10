ffcdac57994291bd34f15d423c932c564129b9bd
SimVil <simon.vilmin@gmail.com>
2017-02-10 04:34:11 +0100

    added a bit of doc to implementation

--- PokemonDataBaseAccessLayer/DALImplementation.cs
+++ PokemonDataBaseAccessLayer/DALImplementation.cs
@@ -28,6 +28,8 @@ namespace PokemonDataBaseAccessLayer
 
         public DALImplementation() { }
 
+        // maniere generique de recuperer une liste de string quelque soit la donnee 
+        // voulue (Pokemon, Match, Stade, ...)
         private List<string> DataRequire(string request)
         {
             List<string> res = new List<string>();
@@ -43,6 +45,9 @@ namespace PokemonDataBaseAccessLayer
 
                 while (sqlDaRe.Read())
                 {
+                    // tant qu'il y a des lignes dans le resultat
+                    // on transforme chacune des valeurs de la ligne en string
+                    // et on join le tout, en separant chaque valeur par un ' '
                     for(i = 0; i < sqlDaRe.FieldCount; i++) { tmp.Add(sqlDaRe.GetValue(i).ToString()); }
                     res.Add(string.Join(" ", tmp.ToArray()));
                     tmp.Clear();
@@ -63,6 +68,8 @@ namespace PokemonDataBaseAccessLayer
 
             using (SqlConnection connect = new SqlConnection(_connectionString))
             {
+                // on recupere les donnees demandees par la requete
+                // et on remplit la datatable voulue en fonction
                 SqlCommand com = new SqlCommand(r, connect);
                 SqlDataAdapter adapter = new SqlDataAdapter(com);
                 adapter.Fill(res);
@@ -98,8 +105,9 @@ namespace PokemonDataBaseAccessLayer
 
         private DataTable PkBuild(string s)
         {
-            DataTable t = TableRequire(s);
-            t.PrimaryKey = new DataColumn[] { t.Columns["idp"] };
+            DataTable t = TableRequire(s); // recupere la table demandee par la string
+            t.PrimaryKey = new DataColumn[] { t.Columns["idp"] }; // fixe une clef primaire sur la donnee
+            // (fait echo a la bd en dur)
             return t;
         }
 
@@ -110,8 +118,9 @@ namespace PokemonDataBaseAccessLayer
             int res = 0;
             string r = "select * from Pokemon;";
 
-            DataTable tp = (GetPokemonTable());
+            DataTable tp = GetPokemonTable(); // on recup les pokemon
             bool exist = tp.Rows.Contains(p.id);
+            // le Contains permet de savoir si le pokemon existe deja
 
             if (!exist)
             {
@@ -119,18 +128,31 @@ namespace PokemonDataBaseAccessLayer
                 {
                     using (SqlConnection connect = new SqlConnection(_connectionString))
                     {
+                        // connexion standard
+
+                        // on cree une commande sql a partir de notre requete en string
+                        // et de notre connexion a la bd.
+                        // Ensuite, on cree un adapter qui va faire le lien entre notre
+                        // commande et la bd. 
+                        // Le builder lui sert a faire de la generation automatique de
+                        // commandes pour faire fonctionner l'adapter.
                         connect.Open();
                         SqlCommand cmd = new SqlCommand(r, connect);
                         SqlDataAdapter adapter = new SqlDataAdapter(cmd);
                         SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
 
+                        // necessaire car le adapter.Update necessite les trois
+                        // commande (meme generees par defaut) pour fonctionner.
                         adapter.InsertCommand = builder.GetInsertCommand(true);
                         adapter.UpdateCommand = builder.GetUpdateCommand(true);
                         adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                        // l'ajout se fait ici
+                        // attention, l'ordre des parametres est tres important
                         tp.Rows.Add(p.id, p.Nom, p.Vie, p.Force, p.Defense);
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
 
+                        // on update la table, et sauvegarde les modifs
                         res = adapter.Update(tp);
                         tp.AcceptChanges();
                         connect.Close();
@@ -144,6 +166,8 @@ namespace PokemonDataBaseAccessLayer
 
                 }
 
+                // traitement de la liste de type associe a un pokemon. Pour chaque type, on
+                // ajoute un tuple (idp, type) dans la table PokemonType.
                 foreach (TypeElement x in p.Types) { res = AddPokemonType(p.id, (Int32)x + 1); }
 
             }
@@ -151,7 +175,6 @@ namespace PokemonDataBaseAccessLayer
 
             return res;
         }
-
 
 
         public int UpdatePokemon(Pokemon p)
@@ -177,6 +200,8 @@ namespace PokemonDataBaseAccessLayer
                         adapter.InsertCommand = builder.GetInsertCommand(true);
                         adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                        // la mise a jour est ici. On recupere la ligne, et on en change les 
+                        // valeurs
                         t.Rows.Find(p.id).ItemArray = new object[] { p.id, p.Nom, p.Vie, p.Force, p.Defense };
 
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
@@ -187,31 +212,36 @@ namespace PokemonDataBaseAccessLayer
 
 
                     }
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Console.WriteLine("Erreur dans UpdatePokemon");
                     Console.WriteLine(e.ToString());
 
                 }
 
+                // Bien sur il faut aussi mettre a jour les types !
                 res ^= UpdatePokemonType(p);
 
-            }
-            else
-            {
-                Console.WriteLine("Update : Le pokemon nexiste pas !");
-
-            }
+            } else {  Console.WriteLine("Update : Le pokemon nexiste pas !"); }
             return res;
 
         }
+
+
+
 
         public int DeletePokemon(Pokemon p)
         {
             int res = 0;
             string r = "select * from Pokemon;";
             DataTable t = GetPokemonTable();
+
+            // pour respecter l'odre de composition de la bd, on supprime d'abord
+            // les instances de PokemonType ou des Matchs qui impliquent le poke
+            // que l'on veut supprimer.
+
+            // NB : il existe un mot clef DataTable.ForeignKeyConstraint, mais au
+            // vu de la description dans la doc MSDN, j'ai pas forcement bien compris
+            // si au dela de sa valeur semantique, on aurait pu s'en servir pour ca.
 
             res = DeletePokemonType(p.id);
             res = DeleteMatchByPokemonId(p.id);
@@ -233,6 +263,7 @@ namespace PokemonDataBaseAccessLayer
                         adapter.UpdateCommand = builder.GetUpdateCommand(true);
                         adapter.InsertCommand = builder.GetInsertCommand(true);
 
+                        // on trouve la bonne ligne, et squick la ligne
                         t.Rows.Find(p.id).Delete();
 
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
@@ -273,6 +304,9 @@ namespace PokemonDataBaseAccessLayer
 
         public List<string> GetPokemonTypeById(int id) { return DataRequire("select * from PokemonType where pkm = " + id + ";"); }
 
+
+        // comme plus haut, sert a formater la datatable associee a pokemontype,
+        // en specifiant la clef primaire
         private DataTable PkTypeBuild(string s)
         {
             DataTable t = TableRequire(s);
@@ -280,17 +314,21 @@ namespace PokemonDataBaseAccessLayer
             return t;
         }
 
+        // les 3 requetes dont on a besoin pour faire nos traitements
         private DataTable GetPokemonTypeTable() { return PkTypeBuild("select * from PokemonType;"); }
         
         private DataTable GetPokemonTypeTableById(int id) { return PkTypeBuild("select * from PokemonType where pkm = " + id + ";"); }
 
         private DataTable GetPokemonTypeTableByIdType(int id, int type) { return PkTypeBuild("select * from PokemonType where pkm = " + id + " and type = " + type + ";"); }
 
+
         public int AddPokemonType(int id, int type)
         {
             int res = 0;
             string r = "select * from PokemonType;";
             DataTable t = GetPokemonTypeTableById(id);
+            // le byid permet de recuperer seulement les lignes relatives a un
+            // pokemon (defini par id)
 
             bool exist = t.Rows.Contains(new Object[] { id, type });
 
@@ -309,6 +347,7 @@ namespace PokemonDataBaseAccessLayer
                         adapter.UpdateCommand = builder.GetUpdateCommand(true);
                         adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                        // ici on fait l'ajout
                         t.Rows.Add(id, type);
 
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
@@ -349,6 +388,9 @@ namespace PokemonDataBaseAccessLayer
                     adapter.UpdateCommand = builder.GetUpdateCommand(true);
                     adapter.InsertCommand = builder.GetInsertCommand(true);
 
+                    // pour etre sur de detruire tous les types d'un pokemon, on
+                    // efface toutes les lignes de la table contenant son id
+                    // --> toutes les lignes resultant du get By id
                     for (int i = 0; i < tb.Rows.Count; i++)
                     {
                         tb.Rows[i].Delete();
@@ -373,6 +415,8 @@ namespace PokemonDataBaseAccessLayer
         public int UpdatePokemonType(Pokemon p)
         {
             int res = 0;
+            // ici on utilise une technique de sagouin, mais courte en code
+            // la modification c'est tout virer, et tout remettre.
             res = DeletePokemonType(p.id);
             foreach (TypeElement x in p.Types)
             {
@@ -385,19 +429,25 @@ namespace PokemonDataBaseAccessLayer
 
 
 /* Methodes Stade -----------------------------------------------------
-*  + GetAllStades  ~
-*  + GetStadeById  ~
+*  + GetAllStades  ~ recupere tous les stades
+*  + GetStadeById  ~ recupere un stade par son id
 *  
-*  - GetStadeTable ~ 
+*  - GetStadeTable ~ recupere une table de stade
 * 
-*  + AddStade      ~
-*  + UpdateStade   ~
-*  + DeleteStade   ~
+*  + AddStade      ~ ajoute un stade
+*  + UpdateStade   ~ met a jour un stade
+*  + DeleteStade   ~ detruit un stade
 * 
 * ----------------------------------------------------------------- */
 
         public List<string> GetAllStades() { return DataRequire("select * from Stade;"); }
 
+
+        // la fonction marche bien sous reserve que le stade existe.
+        // si ce n'est pas le cas, le .First() jettera une exception
+
+        // Normalement, ce cas est couvert par les controles au niveau 
+        // du BL (j'espere ...)
         public string GetStadeById(int id)
         {
             string r = "select * from Stade where ids = " + id + ";";
@@ -406,6 +456,9 @@ namespace PokemonDataBaseAccessLayer
             return string.Format(t.First());
         }
 
+
+        // ici on a pas fait de builder, vu qu'il n'y a qu'une seule
+        // methode
         private DataTable GetStadeTable()
         {
             string r = "select * from Stade;";
@@ -419,6 +472,11 @@ namespace PokemonDataBaseAccessLayer
             int res = 0;
             string r = "select * from Stade;";
 
+            // recuperation table, puis verification de la non-existence
+            // du stade avant ajout
+
+            // rmq : on refait la verif en fait, puisqu'on a deja verifie
+            // plus haut
             DataTable tp = GetStadeTable();
             bool exist = tp.Rows.Contains(s.ids);
 
@@ -432,11 +490,17 @@ namespace PokemonDataBaseAccessLayer
                         SqlCommand cmd = new SqlCommand(r, connect);
                         SqlDataAdapter adapter = new SqlDataAdapter(cmd);
                         SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+                        
 
+                        // toujours les 3 commandes obligatoires pour que le update puisse fonctionner.
+                        // Petit point culture : que fait vraiment cette commande ? Si j'ai bien compris
+                        // elle genere automatiquement des commandes du type insert, update et delete
+                        // avec des champs remplis par defaut.
                         adapter.InsertCommand = builder.GetInsertCommand(true);
                         adapter.UpdateCommand = builder.GetUpdateCommand(true);
                         adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                        // lieu de l'ajout
                         tp.Rows.Add(s.ids, s.NbPlaces, s.Nom, (Int32)s.Types.First() + 1);
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
 
@@ -456,13 +520,16 @@ namespace PokemonDataBaseAccessLayer
             return res;
         }
 
+
+
         public int UpdateStade(Stade s) {
             int res = 0;
             string r = "select * from Stade;";
 
-            DataTable t = GetStadeTable();
-            bool exist = t.Rows.Contains(s.ids);
+            DataTable t = GetStadeTable(); // recup table
+            bool exist = t.Rows.Contains(s.ids); // existe ?
 
+            // si oui, on peut mettre a jour !
             if (exist)
             {
                 try
@@ -478,6 +545,7 @@ namespace PokemonDataBaseAccessLayer
                         adapter.InsertCommand = builder.GetInsertCommand(true);
                         adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                        // lieu de la mise a jour
                         t.Rows.Find(s.ids).ItemArray = new object[] {s.ids, s.NbPlaces, s.Nom, (Int32)s.Types.First() + 1};
 
                         adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
@@ -499,14 +567,18 @@ namespace PokemonDataBaseAccessLayer
             return res;
         }
 
+
+
         public int DeleteStade(Stade s)
         {
             int res = 0;
             string r = "select * from Stade;";
             DataTable t = GetStadeTable();
 
+            // /!\ attention ici : de meme que pour la suppression de pokemon
+            // il faut bien faire attention a oter les matchs impliquant le stade
+            // que l'on veut detruire --> difficile de jouer dans un stade inexistant !
             res = DeleteMatchByStadeId(s.ids);
-
             bool exist = t.Rows.Contains(s.ids);
 
             if (exist)
@@ -546,17 +618,17 @@ namespace PokemonDataBaseAccessLayer
         }
 
 
-        /* Methodes Match -----------------------------------------------------
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * ----------------------------------------------------------------- */
+/* Methodes Match -------------------------------------------------------------
+*  + GetAllMatchs ~
+*  - MtBuild
+*  - GetMatchTable
+*  - GetMatchByStadeId
+*  - GetMatchByPokemonId
+*  + Add, Delete, Update
+*  + DeleteMatchByPokemonId ~ permet de supprimer tous les matchs associe a un
+*                             seul pokemon (cf delete pokemon)
+*  + DeleteMatchByStadeId   ~ idem, pour les stades
+* --------------------------------------------------------------------------- */
 
 
         public List<string> GetAllMatchs() { return DataRequire("select * from Match;"); }
@@ -819,6 +891,8 @@ namespace PokemonDataBaseAccessLayer
         
         public List<string> GetAllUtilisateurs() { return DataRequire("select * from Utilisateur;");  }
 
+
+        // recuperation de la table des utilisateurs
         private DataTable GetUtilisateurTable()
         {
             string r = "select * from Utilisateur;";
@@ -827,8 +901,26 @@ namespace PokemonDataBaseAccessLayer
             return t;
         }
 
+
+        // Ici un delegate --> alias de fonction, ça definit en gros un type fonction
+        // que l'on peut appeler, notamment en parametres de methodes. Ca permet donc
+        // de passer des methodes en parametres d'autre.
+        //
+        // ex
+        //  private delegate int add(int, int)
+        //  private int fonction(int a, int b) { return a + b;}
+        //
+        // On pourrait tres bien imaginer un prototype de methode
+        //   public void superajout(int a, int b, add fct)
+        //
+        // Et l'appeler avec
+        //   superajout(5, 7, fonction);
+
         private delegate void AppliedMethod(Utilisateur u, DataTable t);
 
+        // methode permettant de centraliser l'appel a la connection. On passe un
+        // delegate (equivalent d'un alias pour fonction un peu) qui specifie la methode
+        // en fonction de ce que l'on veut faire (insert, delete, update).
         private int UtilisateurCall(Utilisateur u, AppliedMethod meth, DataTable t)
         {
             int res = 0;
@@ -847,6 +939,10 @@ namespace PokemonDataBaseAccessLayer
                     adapter.UpdateCommand = builder.GetUpdateCommand(true);
                     adapter.DeleteCommand = builder.GetDeleteCommand(true);
 
+                    // execute ici la manipulation voulue sur la datatable
+                    // Adder(u, t)   -> insert
+                    // Updater(u, t) -> itemarray
+                    // Deleter(u, t) -> .find.delete 
                     meth(u, t);
                     adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
 
@@ -865,6 +961,9 @@ namespace PokemonDataBaseAccessLayer
             return res;
         }
 
+        // addutilisateur prend un user, cree la table, et passe la methode appropriee
+        // en parametre de la fonction generale, i.e la methode AdderUtilisateur, qui
+        // ajoute un row dans une datatable.
         public int AddUtilisateur(Utilisateur u)
         {
             DataTable tp = GetUtilisateurTable();
@@ -895,6 +994,7 @@ namespace PokemonDataBaseAccessLayer
             return 0;
         }
 
+        // ici les 3 methodes que l'on peut vouloir appeler comme "meth" dans la fonction UtilisateurCall
         private void AdderUtilisateur(Utilisateur u, DataTable t) { t.Rows.Add(u.idu, u.Nom, u.Prenom, u.Login, u.Password); }
 
         private void UpdaterUtilisateur(Utilisateur u, DataTable t) { t.Rows.Find(u.idu).ItemArray = new object[] { u.idu, u.Nom, u.Prenom, u.Login, u.Password }; }
@@ -903,17 +1003,10 @@ namespace PokemonDataBaseAccessLayer
 
 
 
-        /* Methodes Element ---------------------------------------------------
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * ----------------------------------------------------------------- */
+/* Methodes Element -----------------------------------------------------------
+* - GetElementTable
+* + GetAllElements
+* -------------------------------------------------------------------------- */
 
         private DataTable GetElementTable()
         {
